cebef007e08d262a84ce8662e9d5be3f0b0c1790
unknown <eliasniemi72@gamil.com>
2020-02-10 18:58:57 +0200

    endless pin code spammer

---
+++ passwordmaker.bat
@@ -0,0 +1,24 @@
+@echo off
+:a
+cls
+echo %random% >> pass.txt
+echo %random% >> pass.txt
+echo %random% >> pass.txt
+echo %random%%random% >> pass.txt
+echo %random%%random% >> pass.txt
+echo %random%%random% >> pass.txt
+echo %random%%random% >> pass.txt
+echo %random%%random% >> pass.txt
+echo %random%%random%%random% >> pass.txt
+echo %random%%random%%random% >> pass.txt
+echo %random%%random%%random% >> pass.txt
+echo %random%%random%%random% >> pass.txt
+echo %random%%random%%random% >> pass.txt
+echo %random%%random%%random% >> pass.txt
+echo %random%%random%%random%%random% >> pass.txt
+echo %random%%random%%random%%random% >> pass.txt
+echo %random%%random%%random%%random% >> pass.txt
+echo %random%%random%%random%%random% >> pass.txt
+echo %random%%random%%random%%random% >> pass.txt
+echo %random%%random%%random%%random% >> pass.txt
+goto :a
